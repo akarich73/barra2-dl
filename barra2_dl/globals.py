5f41928fe6df30796771c1f42bf3a93ecb08d63a
akarich73 <61820266+akarich73@users.noreply.github.com>
2024-10-14 23:02:09 +1100

    Initial commit

--- barra2_dl/globals.py
+++ barra2_dl/globals.py
@@ -49,10 +49,29 @@ barra2_aus11_csv_url = ("https://thredds.nci.org.au/thredds/ncss/grid/ob53/outpu
                         "{var}_AUS-11_ERA5_historical_hres_BOM_BARRA-R2_v1_1hr_{year}{month:02d}-{year}{month:02d}.nc")
 
 # index for barra2 used to join separate files
-barra2_index = ['time', 'station', 'latitude[unit="degrees_north"]', 'longitude[unit="degrees_east"]']
+barra2_aus11__index = ['time', 'station', 'latitude[unit="degrees_north"]', 'longitude[unit="degrees_east"]']
+
+# BARRA2 wind speed variable pairs
+barra2_aus11_wind_all = [('ua10m', 'va10m', '10m[unit="m s-1"]'),
+                         ('ua20m', 'va20m', '20m[unit="m s-1"]'),
+                         ('ua30m', 'va30m', '30m[unit="m s-1"]'),
+                         ('ua50m', 'va50m', '50m[unit="m s-1"]'),
+                         ('ua70m', 'va70m', '70m[unit="m s-1"]'),
+                         ('ua100m', 'va100m', '100m[unit="m s-1"]'),
+                         ('ua150m', 'va150m', '150m[unit="m s-1"]'),
+                         ('ua200m', 'va200m', '200m[unit="m s-1"]'),
+                         ('ua250m', 'va250m', '250m[unit="m s-1"]'),
+                         ('ua300m', 'va300m', '30m[unit="m s-1"]'),
+                         ('ua400m', 'va400m', '400m[unit="m s-1"]'),
+                         ('ua500m', 'va500m', '500m[unit="m s-1"]'),
+                         ('ua600m', 'va600m', '600m[unit="m s-1"]'),
+                         ('ua700m', 'va700m', '700m[unit="m s-1"]'),
+                         ('ua850m', 'va850m', '850m[unit="m s-1"]'),
+                         ('ua925m', 'va925m', '925m[unit="m s-1"]'),
+                         ('ua1000m', 'va1000m', '1000m[unit="m s-1"]')]
 
 # set list of BARRA2 variables to download default list is eastward wind (ua*), northward wind (va*), and air temperature at 50m (ta50m)
-barra2_var_wind_all = ["ua50m", "va50m", "ua100m", "va100m", "ua150m", "va150m", "ta50m"]
+barra2_var_wind_custom = ["ua50m", "va50m", "ua100m", "va100m", "ua150m", "va150m", "ta50m"]
 
 # optional limited variables to test
 barra2_var_wind_50m = ["ua50m", "va50m", "ta50m"]
